6fa6ce80c7f4813be02bd57d31e3860976274135
gabrielcoder247 <gabrielcoder247@gmail.com>
2022-09-03 15:48:34 +0100

    Create CRUD using JsonPlaceholder Api

---
+++ src/components/AddUser.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
+
+
+
+
+
+
+export const AddUser = ({ onAdd }) => {
+
+  // const [emailError, setEmailError] = useState('')
+  // const validateEmail = (e) => {
+  //   var email = e.target.value
+
+  //   if (validator.isEmail(email)) {
+  //     setEmailError('Valid Email :)')
+  //   } else {
+  //     setEmailError('Enter valid Email!')
+  //   }
+  // }
+
+
+  const handleOnSubmit = (evt) => {
+    evt.preventDefault();
+    onAdd(evt.target.name.value, evt.target.email.value);
+    evt.target.name.value = "";
+    evt.target.email.value = "";
+
+  };
+
+  return (
+    <div className='d-flex flex-column align-items-center'>
+     <Form onSubmit={handleOnSubmit} style={{ width: '20rem', paddingTop: '2rem'}}>
+           <Form.Group>
+               <Form.Label>ADD USER</Form.Label>
+               <Form.Control className="name-input" type="text" placeholder="name" name="name" />
+               <Form.Control className="email-input" type="email" placeholder="email" name="email"/>
+           </Form.Group>
+           <Button onSubmit={handleOnSubmit} className="submit-button" value="submit" type='submit' style={{marginTop: '2rem'}}>Submit Form</Button>
+       </Form>
+    </div>
+  );
+
+};
---
+++ src/components/User.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
+export const User = ({ name, email, id, onEdit, onDelete }) => {
+  const [isEdit, setIsEdit] = useState(false);
+
+  const handleEdit = () => {
+    setIsEdit(!isEdit);
+  };
+
+  const handleDelete = () => {
+    onDelete(id);
+  };
+
+  const handleOnEditSubmit = (evt) => {
+    evt.preventDefault();
+    onEdit(id, evt.target.name.value, evt.target.email.value);
+    setIsEdit(!isEdit);
+  };
+
+  return (
+    <div>
+      {isEdit ? (
+       <div className='d-flex flex-column align-items-center'>
+       <Form onSubmit={handleOnEditSubmit} style={{ width: '20rem', paddingTop: '2rem'}}>
+             <Form.Group>
+                 <Form.Label>ADD USER</Form.Label>
+                 <Form.Control className="name-input" type="text" placeholder="name" name="name" />
+                 <Form.Control className="email-input" type="email" placeholder="email" name="email"/>
+             </Form.Group>
+             <Button onSubmit={handleOnEditSubmit} className="submit-button" value="submit" type='submit' style={{marginTop: '2rem'}}>Submit Form</Button>
+         </Form>
+      </div>
+      ) : (
+        <div className='user d-flex flex-column align-items-center'>
+          <span className="user-name">{name}</span>
+          <span className="user-email">{email}</span>
+          <div>
+            <Button onClick={handleEdit}>Edit</Button>
+            <Button onClick={handleDelete}>Delete</Button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+
