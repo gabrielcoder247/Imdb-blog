f11989869ea2680b72cc22c3c2268ba3424940a2
gabrielcoder247 <gabrielcoder247@gmail.com>
2022-09-06 10:39:50 +0100

    Fix bugs in the chart Api

--- src/components/chart.jsx
+++ src/components/chart.jsx
@@ -1,65 +1,81 @@
-
-import React, {useState,useEffect} from "react";
-import Chart from "chart.js/auto";
+import React, { useState, useEffect } from "react";
 import { Bar } from "react-chartjs-2";
-import  Navbar  from "./Navbar";
+import Navbar from "./Navbar";
 
 const BarChart = () => {
     const [endpoint, setEndpoint] = useState('')
     const [container, setContainer] = useState([])
+    const [storeChart, setStoreChart] = useState('');
+
+
 
     useEffect(() => {
-      fetchData()
-    },[])
-    const fetchData = () => {
-      const options = {
-        method: 'GET',
-        headers: {
-          'X-RapidAPI-Key': '4bd952d73bmsh8bf9cd7faf39a60p12ebe2jsne830ef41f37f',
-          'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
-        }
-      };
+        fetchData()
+        return window.localStorage.setItem("storeChart", result)
 
-      fetch('https://imdb8.p.rapidapi.com/auto-complete?q=game%20of%20thr', options)
-        .then(response => response.json())
-        .then(data => setContainer(data))
-        .catch(err => console.error(err));
+    }, [])
+
+    useEffect(() => {
+        const clientId = () => {
+            return localStorage.getItem("storeChart")
+        }
+
+        if (storeChart) {
+            setStoreChart(storeChart);
+        }
+    }, []);
+    const fetchData = async() => {
+        const options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': '4bd952d73bmsh8bf9cd7faf39a60p12ebe2jsne830',
+                'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
+            }
+        };
+
+        const fetchResult = await fetch('https://imdb8.p.rapidapi.com/auto-complete?q=game%20of%20thr', options)
+            .then(response => response.json())
+            .then(data => setContainer(data))
+            .catch(err => console.error(err));
+
+        // console.log(fetchResult)
 
     }
 
-    console.log(container)
-    let result = []
-  const labelValue = Object.keys(container.d)
-  const dataValue = Object.values(container.d)
-  const finalValue = dataValue.map((e) => result.push(e.y))
-  result.sort((a,b)=> a-b)
+    // console.log(container.d)
+    // let result = []
+        // // const labelValue = Object.keys(container.d)
+        // const dataValue = Object.values(container.d)
+        // const finalValue = dataValue.map((e) => result.push(e.y))
+        // const finalResult = result.sort((a,b)=> a-b)
+        // console.log(finalResult)
+        const newArr = container.d
+        console.log(newArr)
+        const result  = newArr.map((e) => e.y)
 
 
 
 
-  const labels = ["January", "February", "March", "April", "May", "June"];
+    const labels = ["January", "February", "March", "April", "May", "June"];
 
-  const data = {
-    labels: result,
-    // labels: labels,
+    const data = {
+        // labels: labels,
+        labels:result,
 
-    datasets: [
-      {
-        label: container.q,
-        // label: 'Hello',
-        backgroundColor: "rgb(255, 99, 132)",
-        borderColor: "rgb(255, 99, 132)",
-        // data: dataValue,
-        data: [0, 10, 5, 2, 20, 30, 45],
-      },
-    ],
-  };
-  return (
-    <div>
-      <Navbar/>
-      <Bar data={data} />
-    </div>
-  );
+        datasets: [{
+            label: container,
+            // label: 'Hello',
+            backgroundColor: "rgb(255, 99, 132)",
+            borderColor: "rgb(255, 99, 132)",
+            // data: dataValue,
+            data: [0, 10, 5, 2, 20, 30, 45],
+        }, ],
+    };
+    return ( <div>
+        <Navbar />
+        <Bar data = { data }/>
+        </div>
+    );
 };
 
 export default BarChart;
